b42abdf66853b962e6ea6a92ff9287d180f8f591
aymanesarrar <aysarrar@student.1337.ma>
2022-01-09 19:53:15 +0100

    testing user commits

--- srcs/map_validation2.c
+++ srcs/map_validation2.c
@@ -6,12 +6,12 @@
 /*   By: aysarrar <aysarrar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 10:34:38 by aysarrar          #+#    #+#             */
-/*   Updated: 2022/01/09 19:51:28 by aysarrar         ###   ########.fr       */
+/*   Updated: 2022/01/09 19:53:09 by aysarrar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
-//test
+//testt
 char    **get_map(int fd)
 {
     char    *full_map;
